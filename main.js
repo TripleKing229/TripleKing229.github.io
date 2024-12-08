8d7a8f83b098c35390f3959aa79a3bd390eded10
TripleKing229 <harrisontgrant@gmail.com>
2024-12-08 13:49:57 -0800

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function updateResources() {
 
     // Increment resources each second by the growth rate, capped at resourceCap
     if (resources < resourceCap) {
-        resources += resourceGrowthRate;
+        resources += resourceGrowthRate * resources;
         if (resources > resourceCap) {
             resources = resourceCap;
         }
